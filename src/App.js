caeaada9f98413ec2368b3410ed3c8bbf050e90b
Rabinbhatta <rabinbhattarai646@gmail.com>
2025-04-08 15:24:01 +0545

    url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:4000");
+const socket = io("https://message-backend-a06d.onrender.com");
 
 function App() {
   const [messages, setMessages] = useState([]);
